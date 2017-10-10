ae0286783eec3e1668b2d179a1b3a2ce63bdd34d
Elin Axelsson <elinaxel@gmail.com>
2017-10-10 17:15:47 +0200

    trying to post forskalle

--- copf2/copf_functions.py
+++ copf2/copf_functions.py
@@ -21,3 +21,34 @@ def read_json(jsonf):
     with open(jsonf, 'r') as json_file:
         data = json.load(json_file)
     return data
+
+
+def correctforskalle(sample_id,**corrections):
+    passw='zBLOf2@7'
+    user='Elin.Axelsson'
+    s = requests.Session()
+    auth = { 'username': user , 'password': passw }
+    r = s.post('http://ngs.csf.ac.at/forskalle/api/login', data=auth)
+    if (r.status_code != 200):
+        raise Exception('Authentication error?')
+    r = s.get('http://ngs.csf.ac.at/forskalle/api/samples/'+str(sample_id))
+    sample = r.json()
+    for kw in corrections:
+        if sample[kw] == corrections[kw]:
+            print "same"
+        else:
+            print "got: " + sample[kw]
+            print "want: " + corrections[kw]
+            sample['tissue_type'] = unicode("Seedlings 12 days old")
+            print sample['tissue_type']
+            print sample
+            res=s.post('http://ngs.csf.ac.at/forskalle/api/samples/'+str(sample_id), data=json.dumps(sample))
+            print res.status_code
+#print res.json()['tissue_type']
+
+#print res.json()['tissue_type'] ###
+
+#print sample
+# for kw in corrections:
+#       print kw
+#       print sample[kw] + " : " +
